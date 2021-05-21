e54c0a5565597f9b18f577af574e6e605c9bef83
Xichao <>
2021-05-21 22:12:46 +0000

    chapter 3

--- work/chapter3.py
+++ work/chapter3.py
@@ -1,4 +1,5 @@
-# task 1
+from random import randint
+
 def task1():
   length = float(input('length: '))
   width = float(input('width: '))
@@ -12,7 +13,42 @@ def task1():
 
 def task2():
   print('''Menu
-  1. Music ''')
+  1. Music 
+  2. History
+  3. Design and technology
+  4. exit
+  please enter your choice''')
+
+  choices = {
+    1 :'Music'
+    2 :'History'
+    3 :'Design and technology' 
+  }
+
+  user_choice = int(input())
+  if user_choice != 4:
+    print(choices[user_choice])
+  else:
+    print('goodbye')
+
+def task3():
+  dice1 = radom.randint(1,6)
+  dice2 = random.randint(1,6)
+
+  print('you got',dice1, dice2)
+  if dice1 == dice2:
+    score = dice1 + dice2
+    score *= 2
+
+    print('you scored double! your score is', score)
+
+  else: 
+    score = dice1 + dice2
+
+    print('your score is',score) 
+
+def task4():
+  
 
 
 
